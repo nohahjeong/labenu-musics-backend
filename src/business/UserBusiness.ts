5b12e057be5dddc494e3e3b207b2b15c5d144f67
Noh Ah Jeong <noh_ah@hotmail.com>
2021-02-24 21:12:52 -0300

    error handling for signup and login

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -3,6 +3,8 @@ import { IdGenerator } from './services/IdGenerator'
 import { HashManager } from './services/HashManager'
 import { Authenticator } from './services/Authenticator'
 import { LoginInputDTO, SignupInputDTO, User, UserRole } from './model/User'
+import { ExpectationFailedError } from './error/ExpectationFailedError'
+import { NotFoundError } from './error/NotFoundError'
 
 export class UserBusiness {
     constructor(
@@ -13,20 +15,20 @@ export class UserBusiness {
     ) { }
 
     async createUser(user: SignupInputDTO) {
-        if (!user.name || !user.email || !user.password) {
-            throw new Error('Missing input')
+        if (!user.name || !user.email || !user.password || !user.nickname) {
+            throw new ExpectationFailedError('Missing input')
         }
 
         if (user.email.indexOf("@") === -1) {
-           throw new Error('Address must have an @')
+           throw new ExpectationFailedError('Address must have an @')
         }
   
         if (user.password.length < 6) {
-           throw new Error('Password must have at least 6 characters')
+           throw new ExpectationFailedError('Password must have at least 6 characters')
         }
   
         if (user.role !== UserRole.ADMIN && user.role !== UserRole.NORMAL) {
-           throw new Error('User role must be "NORMAL" or "ADMIN"')
+           throw new ExpectationFailedError('User role must be "NORMAL" or "ADMIN"')
         }
 
         const userId = this.idGenerator.generateId()
@@ -50,18 +52,18 @@ export class UserBusiness {
 
     async authUserByEmail(user: LoginInputDTO) {
         if (!user.email || !user.password) {
-            throw new Error('Missing "email" and/or "password"')
+            throw new ExpectationFailedError('Missing "email" and/or "password"')
         }
 
         const userFromDB = await this.userDatabase.getUserByEmail(user.email)
         console.log(userFromDB)
         if (!userFromDB) {
-            throw new Error('Email not registered')
+            throw new NotFoundError('Email not registered')
         }
         const isPasswordCorrect = await this.hashManager.compareHash(user.password, userFromDB.password)
 
         if (!isPasswordCorrect) {
-            throw new Error('Wrong password')
+            throw new ExpectationFailedError('Wrong password')
         }
 
         const acessToken = this.authenticator.generateToken({
